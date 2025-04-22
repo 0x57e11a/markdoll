c6bf2bd3934e4fcf3af21c8ec820859a600f3850
noe <git@kat.cafe>
2025-04-22 10:54:19 -0700

    flake: allow .override { danger = true; } to build with danger feature

--- flake.nix
+++ flake.nix
@@ -47,13 +47,12 @@
           ];
         };
 
-        markdoll = craneLib.buildPackage (commonArgs // {
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-          # Additional environment variables or build phases/hooks can be set
-          # here *without* rebuilding all dependency crates
-          # MY_CUSTOM_VAR = "some value";
-        });
+        markdoll = pkgs.lib.customisation.makeOverridable ({ danger ? false, }: 
+            craneLib.buildPackage (commonArgs // {
+                cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+                cargoExtraArgs = pkgs.lib.optionalString danger "--features danger";
+            })
+        ) { danger = false; };
       in
       {
         checks = {
@@ -72,14 +71,6 @@
         devShells.default = craneLib.devShell {
           # Inherit inputs from checks.
           checks = self.checks.${system};
-
-          # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-
-          # Extra inputs can be added here; cargo and rustc are provided by default.
-          packages = [
-            # pkgs.ripgrep
-          ];
         };
       });
 }
